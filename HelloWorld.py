47f25d7f833b66765eac009be707335f0d8c5d97
Duy Anh <duyanhdao297@gmail.com>
2025-05-28 14:45:11 +0700

    Hello world python

---
+++ HelloWorld.py
@@ -0,0 +1,22 @@
+print("Hello, World!")
+x = 10
+y = 3.14
+
+name = "Alice"
+is_Adult = True
+
+a = 7 
+b = 3
+
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b) 
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello, ",name,"! You are ", age, " years old.")
+
