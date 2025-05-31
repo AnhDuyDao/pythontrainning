2ad0258688006664a60d8af9dd47d24616a70741
Duy Anh <duyanhdao297@gmail.com>
2025-05-31 23:39:27 +0700

    List constructor

---
+++ PythonLists.py
@@ -0,0 +1,22 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+# Changeable
+# Allows duplicate values
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+# List length
+print(len(thislist))
+
+# List items - Data types
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+# Mixed data types
+list4 = ["apple", 1, True]
+
+# List constructor
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
