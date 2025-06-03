35fa33da59cdd391e2415b77eacc045ff34413dc
Duy Anh <duyanhdao297@gmail.com>
2025-06-03 23:36:15 +0700

    Python lists access item 1

--- PythonLists.py
+++ PythonLists.py
@@ -20,3 +20,10 @@ list4 = ["apple", 1, True]
 # List constructor
 thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
 print(thislist)
+
+#Access Items
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])  # Output: banana
+print(thislist[-1]) # Output: cherry (last item)
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
